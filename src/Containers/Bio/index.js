4e91d3aabe775a84c331fd400a66712686414bb5
David Stephan <djstephan21@gmail.com>
2018-11-26 07:10:27 -0600

    added my story

--- src/Containers/Bio/index.js
+++ src/Containers/Bio/index.js
@@ -39,10 +39,35 @@ class Bio extends Component {
                     <BioDiv>
                         <h1>My story</h1>
                         <p><img src={professional} style={professionalImage} />
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit libero at velit fermentum, quis varius magna faucibus. Nullam placerat tempor quam, at pellentesque nisi rutrum a. Cras ut eros mollis, accumsan dui et, aliquet nunc. Ut porttitor nec quam eget dignissim. Maecenas vulputate in neque at consectetur. Nullam dignissim ut neque sit amet accumsan. Donec sodales velit vel erat iaculis, nec dictum mi commodo. Fusce malesuada ullamcorper turpis vitae sollicitudin. Sed rhoncus justo eu risus convallis pulvinar. Vestibulum at arcu ac metus facilisis efficitur. Curabitur malesuada sit amet purus eu sollicitudin. Donec viverra massa eget enim efficitur, in bibendum felis tincidunt. Nunc feugiat arcu id dolor scelerisque blandit sed quis sapien. Donec rutrum velit id turpis tristique tincidunt vulputate ut felis. Donec in nibh ac neque egestas bibendum vitae cursus turpis. Fusce ornare lacus at lobortis posuere.
-                                </p>
+                        I grew up in a small tourist town in Michigan. One of my first jobs was at at a boat rental, 
+                        where I spent seven summers and eventually became the manager. During my time at the boat 
+                        rental I learned what anyone who’s own a boat figures out, they break, and often. But, when 
+                        you rent those boats for a living it’s not really feasible to take a boat to a mechanic every 
+                        time an issue arises, so you learn to fix things. During my time at the boat rental I also got 
+                        interested in motorcycles, and quickly discovered that just like boats, they required a 
+                        considerable amount of maintenance, and seeing as how I didn’t have much by the way of finances, 
+                        if I wanted to keep riding I would need to teach myself how to fix them. During this time I learn 
+                        that I probably like working on motorcycles just as much as I liked riding them.                                
+                        </p>
                         <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque hendrerit libero at velit fermentum, quis varius magna faucibus. Nullam placerat tempor quam, at pellentesque nisi rutrum a. Cras ut eros mollis, accumsan dui et, aliquet nunc. Ut porttitor nec quam eget dignissim. Maecenas vulputate in neque at consectetur. Nullam dignissim ut neque sit amet accumsan. Donec sodales velit vel erat iaculis, nec dictum mi commodo. Fusce malesuada ullamcorper turpis vitae sollicitudin. Sed rhoncus justo eu risus convallis pulvinar. Vestibulum at arcu ac metus facilisis efficitur. Curabitur malesuada sit amet purus eu sollicitudin. Donec viverra massa eget enim efficitur, in bibendum felis tincidunt. Nunc feugiat arcu id dolor scelerisque blandit sed quis sapien. Donec rutrum velit id turpis tristique tincidunt vulputate ut felis. Donec in nibh ac neque egestas bibendum vitae cursus turpis. Fusce ornare lacus at lobortis posuere.
+                        Knowing that I was good at math and that I enjoyed wrenching I decided to pursue a degree in 
+                        Mechanical Engineering at Michigan Technological University. During my time at MTU I discovered 
+                        that I liked writing code, and would take every opportunity in my schoolwork I could to do so. 
+                        In my last semester I got to enroll in an autonomous systems class. The class was based around a 
+                        semester long project where we programmed small robots to navigate a small town. My robot had to 
+                        be able to stay in its lane, stop at stop signs and lights, and of course, avoid running into the 
+                        town's residents, rubber ducks. During this class I got to see how I could write code and have that 
+                        code control other objects. I could use this code to obtain information from sensors and make 
+                        decisions about how to turn the motors. I liked this experience so much I began to look into careers 
+                        in software development and quickly found that the principles of object oriented programming were very 
+                        similar to what I was doing with the robot, but instead of interacting with physical objects like 
+                        sensors and motors, you could interact with objects in your own software, that you get to design!                        
+                        </p>
+                        <p>
+                        In my investigation of software development I quickly learn that there were many concepts and technologies 
+                        that I didn’t have knowledge of, and that’s when I found Cook System’s and their FastTrack’D developer 
+                        bootcamp. At FastTrack’D I had the opportunity to learn not only languages like Java and JavaScript but also 
+                        the Spring framework,  React, and Redux.
                         </p>
                     </BioDiv>
 
